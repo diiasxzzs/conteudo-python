d2ac02c10e83b30a40c4120f4152818cc597ee77
YASMIN ALESSANDRA CAVALCANTE DIAS <yasmin_ac_dias@estudante.sesisenai.org.br>
2025-10-03 11:18:55 -0300

    aula 2: variaveis e tipos

---
+++ aula 2.py
@@ -0,0 +1,25 @@
+# trabalhando com variaveis
+from linecache import clearcache
+
+#definindo uma variavel com o meu nome
+nome = ("Yasmin")
+print ("o nome e", nome)
+
+palavra="xulofimpola"
+letra = "a"
+quantidade = 10
+valor = 10.5
+aberto = False
+
+print (" o valor de palavra e", palavra,end=" ")
+print ("( tipo :", type (letra),")")
+
+print  ("o valor de letra e" , letra)
+print ("(tipo : ", type(letra), end = "")
+
+print (" o valor de quantidade e", quantidade,end="")
+print ("( tipo : ",type (letra),")")
+print ("o valor de valor e", valor)
+
+print ("o valor de aberto e", aberto)
+
