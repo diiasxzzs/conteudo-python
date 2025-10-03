a95684018adc41a31344f5ed4b8c05e8f0bdb50f
YASMIN ALESSANDRA CAVALCANTE DIAS <yasmin_ac_dias@estudante.sesisenai.org.br>
2025-10-03 09:49:35 -0300

    aula1: comando print

---
+++ aula1.py
@@ -0,0 +1,14 @@
+#isso e um comentario
+# vamosescapar da maldicao
+print ("ola mundo!")
+#podemos mudar o comportamento da finalizacao
+#do comando com o parametro end
+
+print ("esse e o curso de ",end=" ")
+print ("logica", "de", "programcao")
+#podemos mudar o comportamento do separador
+# com 
+
+print ("frase", "com", "hifen" ,sep="-")
+
+
